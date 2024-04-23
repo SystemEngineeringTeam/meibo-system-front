40444d3520cd39abd0fbe28a82d04128c668e428
satooru65536 <ryuto4619@gmail.com>
2024-04-23 15:52:05 +0900

    refactor: global style の定義を変更

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -2,16 +2,30 @@ import { App, ConfigProvider, Layout } from 'antd';
 import { type ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
 import 'the-new-css-reset/css/reset.css';
-import '@/styles/global.css';
 import '@fontsource-variable/noto-sans-jp';
+import { createGlobalStyle } from 'styled-components';
 import Header from '@/components/shared/Header';
 import theme from '@/utils/theme';
 
 const { Header: HeaderContainer, Content } = Layout;
 
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 1.125rem;
+  }
+
+  @media (width < 768px) {
+    html {
+      font-size: 1rem;
+      scroll-padding-top: 60px; /* header height */
+    }
+  }
+`
+
 const BaseLayout = (): ReactElement => (
   <ConfigProvider theme={theme}>
     <App>
+      <GlobalStyle />
       <Layout>
         <HeaderContainer>
           <Header />
