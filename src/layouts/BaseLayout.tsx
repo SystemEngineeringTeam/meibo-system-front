6f147700472c44d8087202656a80a6560aaf10e0
satooru65536 <ryuto4619@gmail.com>
2024-04-23 21:42:25 +0900

    fix: フォントのインポートを削除

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -2,7 +2,6 @@ import { App, ConfigProvider, Layout } from 'antd';
 import { type ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
 import 'the-new-css-reset/css/reset.css';
-import '@fontsource-variable/noto-sans-jp';
 import { createGlobalStyle } from 'styled-components';
 import Header from '@/components/shared/Header';
 import { theme } from '@/utils/theme';
