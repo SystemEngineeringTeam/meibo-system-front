3317dc22d535dff80c995ac547d959fe48abfac1
satooru65536 <ryuto4619@gmail.com>
2024-05-28 18:01:52 +0900

    fix: min-height を指定

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -1,18 +1,23 @@
 import { useAtomValue } from 'jotai';
 import { type ReactElement } from 'react';
+import styled from 'styled-components';
 import TopBody from './body';
 import { userAtomLoadable } from '@/stores/usersAtom';
+
+const Main = styled.main`
+  min-height: 100vh;
+`;
 
 const TopPage = (): ReactElement => {
   const users = useAtomValue(userAtomLoadable);
 
   return (
-    <main>
+    <Main>
       {users.state === 'hasError' && <p>エラー</p>}
       {users.state === 'loading' && <p>ローディング中</p>}
 
       {users.state === 'hasData' && <TopBody users={users.data} />}
-    </main>
+    </Main>
   );
 };
 
