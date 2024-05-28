31e2312dc6b89b5effb3f26905e78ac6fea3e460
satooru65536 <ryuto4619@gmail.com>
2024-05-29 00:06:58 +0900

    Revert "fix: min-height を指定"
    This reverts commit 3317dc22d535dff80c995ac547d959fe48abfac1.

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -1,23 +1,18 @@
 import { useAtomValue } from 'jotai';
 import { type ReactElement } from 'react';
-import styled from 'styled-components';
 import TopBody from './body';
 import { userAtomLoadable } from '@/stores/usersAtom';
-
-const Main = styled.main`
-  min-height: 100vh;
-`;
 
 const TopPage = (): ReactElement => {
   const users = useAtomValue(userAtomLoadable);
 
   return (
-    <Main>
+    <main>
       {users.state === 'hasError' && <p>エラー</p>}
       {users.state === 'loading' && <p>ローディング中</p>}
 
       {users.state === 'hasData' && <TopBody users={users.data} />}
-    </Main>
+    </main>
   );
 };
 
